436efebf248ea54d25db4c0b1ba27fd4b4686b77
Silvar <kinaho2013@gmail.com>
2024-12-02 13:14:02 +1100

    update section

--- src/sections/SkillSection.jsx
+++ src/sections/SkillSection.jsx
@@ -11,70 +11,50 @@ export default function SkillSection() {
             seamless functionality.
           </p>
           <div className="flex flex-col gap-8 font-Poppins font-semibold">
+            {/* Languages and Tools Section */}
             <div className="flex flex-col gap-5">
               <p>Languages and Tools</p>
               <div className="flex flex-rows flex-wrap w-10/12 gap-5">
-                {[
-                  { name: "html", extra: "bg-[length:50px_57px]" },
-                  { name: "css" },
-                  { name: "javascript" },
-                  { name: "vite" },
-                  { name: "node" },
-                  { name: "php" },
-                  { name: "postman" },
-                  { name: "figma" },
-                ].map(({ name, extra = "" }, index) => (
-                  <div
-                    key={index}
-                    className={`group bg-${name} bg-contain bg-center bg-no-repeat aspect-square w-16 ${extra}`}
-                  ></div>
-                ))}
+                <div className="group bg-html  bg-center bg-no-repeat aspect-square w-16 bg-[length:50px_57px]"></div>
+                <div className="group bg-css bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-javascript bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-vite bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-node bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-php bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-postman bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-figma bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
               </div>
             </div>
+
+            {/* Frameworks and Libraries Section */}
             <div className="flex flex-col gap-5">
               <p>Frameworks and Libraries</p>
               <div className="flex flex-rows flex-wrap w-10/12 gap-5">
-                {[
-                  { name: "react" },
-                  {
-                    name: "express",
-                    extra: "bg-white bg-[length:70%_60%]  rounded-full",
-                  },
-                  { name: "tailwind" },
-                  { name: "cakePHP" },
-                  { name: "gsap" },
-                ].map(({ name, extra = "" }, index) => (
-                  <div
-                    key={index}
-                    className={`group bg-${name} bg-contain bg-center bg-no-repeat aspect-square w-16 ${extra}`}
-                  ></div>
-                ))}
+                <div className="group bg-react bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-express bg-white bg-[length:70%_60%] bg-center bg-no-repeat aspect-square w-16 rounded-full"></div>
+                <div className="group bg-tailwind bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-cakePHP bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                <div className="group bg-gsap bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
               </div>
             </div>
+
+            {/* Databases Section */}
             <div className="flex flex-row gap-5">
               <div className="flex flex-col gap-5">
                 <p>Databases</p>
                 <div className="flex flex-rows flex-wrap gap-5">
-                  {["mysql", "mongodb", "postgres"].map((db, index) => (
-                    <div
-                      key={index}
-                      className={`group bg-${db} bg-contain bg-center bg-no-repeat aspect-square w-16`}
-                    ></div>
-                  ))}
+                  <div className="group bg-mysql bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                  <div className="group bg-mongodb bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                  <div className="group bg-postgres bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
                 </div>
               </div>
+
+              {/* Others Section */}
               <div className="flex flex-col gap-5">
                 <p>Others</p>
                 <div className="flex flex-rows flex-wrap gap-5">
-                  {[
-                    { name: "git" },
-                    { name: "spotify", extra: "bg-[length:160%_80%]" },
-                  ].map(({ name, extra = "" }, index) => (
-                    <div
-                      key={index}
-                      className={`group bg-${name} bg-contain bg-center bg-no-repeat aspect-square w-16 ${extra}`}
-                    ></div>
-                  ))}
+                  <div className="group bg-git bg-contain bg-center bg-no-repeat aspect-square w-16"></div>
+                  <div className="group bg-spotify bg-center bg-no-repeat aspect-square w-16 bg-[length:160%_80%]"></div>
                 </div>
               </div>
             </div>
@@ -84,7 +64,6 @@ export default function SkillSection() {
       <div className="flex items-center justify-center shrink">
         <div>
           Animation
-          
         </div>
       </div>
     </div>
