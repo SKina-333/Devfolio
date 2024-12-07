782a67a5603f71088757c015020165a69492912d
Silvar <kinaho2013@gmail.com>
2024-12-07 22:43:26 +1100

    redo looping animation with my own code

--- src/sections/MainSection.jsx
+++ src/sections/MainSection.jsx
@@ -11,118 +11,37 @@ export default function MainSection() {
 
   useGSAP(
     () => {
-
-      function horizontalLoop(items, config) {
-        items = gsap.utils.toArray(items);
-        config = config || {};
-        let tl = gsap.timeline({
-            repeat: config.repeat,
-            paused: config.paused,
-            defaults: { ease: "none" },
-            onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
-          }),
-          length = items.length,
-          startX = items[0].offsetLeft,
-          times = [],
-          widths = [],
-          xPercents = [],
-          curIndex = 0,
-          pixelsPerSecond = (config.speed || 1) * 100,
-          snap = config.snap === false ? (v) => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-          totalWidth,
-          curX,
-          distanceToStart,
-          distanceToLoop,
-          item,
-          i;
-        gsap.set(items, {
-          // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-          xPercent: (i, el) => {
-            let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
-            xPercents[i] = snap(
-              (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
-                gsap.getProperty(el, "xPercent")
-            );
-            return xPercents[i];
-          },
-        });
-        gsap.set(items, { x: 0 });
-        totalWidth =
-          items[length - 1].offsetLeft +
-          (xPercents[length - 1] / 100) * widths[length - 1] -
-          startX +
-          items[length - 1].offsetWidth *
-            gsap.getProperty(items[length - 1], "scaleX") +
-          (parseFloat(config.paddingRight) || 0);
-        for (i = 0; i < length; i++) {
-          item = items[i];
-          curX = (xPercents[i] / 100) * widths[i];
-          distanceToStart = item.offsetLeft + curX - startX;
-          distanceToLoop =
-            distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-          tl.to(
-            item,
-            {
-              xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
-              duration: distanceToLoop / pixelsPerSecond,
-            },
-            0
-          )
-            .fromTo(
-              item,
-              {
-                xPercent: snap(
-                  ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
-                ),
-              },
-              {
-                xPercent: xPercents[i],
-                duration:
-                  (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
-                immediateRender: false,
-              },
-              distanceToLoop / pixelsPerSecond
-            )
-            .add("label" + i, distanceToStart / pixelsPerSecond);
-          times[i] = distanceToStart / pixelsPerSecond;
-        }
-        function toIndex(index, vars) {
-          vars = vars || {};
-          Math.abs(index - curIndex) > length / 2 &&
-            (index += index > curIndex ? -length : length); // always go in the shortest direction
-          let newIndex = gsap.utils.wrap(0, length, index),
-            time = times[newIndex];
-          if (time > tl.time() !== index > curIndex) {
-            // if we're wrapping the timeline's playhead, make the proper adjustments
-            vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
-            time += tl.duration() * (index > curIndex ? 1 : -1);
-          }
-          curIndex = newIndex;
-          vars.overwrite = true;
-          return tl.tweenTo(time, vars);
-        }
-        tl.next = (vars) => toIndex(curIndex + 1, vars);
-        tl.previous = (vars) => toIndex(curIndex - 1, vars);
-        tl.current = () => curIndex;
-        tl.toIndex = (index, vars) => toIndex(index, vars);
-        tl.times = times;
-        tl.progress(1, true).progress(0, true); // pre-render for performance
-        if (config.reversed) {
-          tl.vars.onReverseComplete();
-          tl.reverse();
-        }
-        return tl;
-      }
-      const items = gsap.utils.toArray(container.current.children); // Get all child elements
-      horizontalLoop(items, { repeat:-1 ,speed:1}); // Adjust duration as needed
+      const tl = gsap.timeline({ repeat: 1, repeatDelay: 2});
+      tl.fromTo(
+        "#containerOne", 
+        { x: '-100%' }, 
+        {
+          x: '100%',
+          duration: 35, 
+          repeat: -1, 
+          ease: "none",
+          
+        },
+        
+      );
+      tl.fromTo(
+        "#containerTwo", 
+        { x: '-100%' }, 
+        {
+          x: '100%',
+          duration: 35, 
+          repeat: -1, 
+          ease: "none", 
+        },"-=16"
+      );
     },
     { scope: container }
   );
   return (
-    <div className="lg:h-screen  flex flex-col w-full lg:gap-0 gap-64 mb-24">
+    <div className="lg:h-screen overflow-x-hidden text-nowrap flex flex-col w-full lg:gap-0 gap-64 mb-24">
       <NavComponent />
       <section
-        ref={container}
+        
         className="grow flex flex-col justify-center items-center"
       >
         <h3 className="font-Poppins text-center cursor-default">
@@ -146,14 +65,25 @@ export default function MainSection() {
           </span>
         </p>
       </section>
-      <div
-        ref={container}
-        className="text-center w-full 2xl:text-9xl 2xl:gap-80 lg:text-6xl text-4xl font-bold text-gray-600 text-nowrap overflow-hidden py-5 flex flex-row gap-20 will-change-transform"
-      >
-        <p className="2xl:pl-80 lg:pl-36 pl-20">Front-end developer. </p>
-        <p>Back-end developer. </p>
-        <p>UI/UX Design.</p>
-        <p className="">Animation.</p>
+      <div ref={container} className="flex flex-row overflow-x-hidden text-nowrap w-full relative border border-purple-50 2xl:h-36 lg:h-20 h-10">
+        <div
+          id="containerOne"
+          className="absolute border border-white text-nowrap overflow-hidden flex flex-row 2xl:text-9xl 2xl:gap-80 gap-20 lg:text-6xl text-4xl font-bold text-gray-600 will-change-transform "
+        >
+          <p className="">Front-end developer. </p>
+          <p className="">Back-end developer. </p>
+          <p className="">UI/UX Design.</p>
+          <p className="pr-96">Animation.</p>
+        </div>
+        <div
+          id="containerTwo"
+          className="absolute border border-green-500 text-nowrap overflow-hidden flex flex-row 2xl:text-9xl 2xl:gap-80 gap-20 lg:text-6xl text-4xl font-bold text-gray-600 will-change-transform "
+        >
+          <p className="pl-96">Front-end developer. </p>
+          <p className="">Back-end developer. </p>
+          <p className="">UI/UX Design.</p>
+          <p className="">Animation.</p>
+        </div>
       </div>
     </div>
   );
