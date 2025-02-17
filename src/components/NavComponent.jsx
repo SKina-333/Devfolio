abb0a5a9f6c5e6ec30e68abc50399f38b2a3d0b9
Silvar <kinaho2013@gmail.com>
2025-02-17 23:16:31 +1100

    changing image config

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
 export default function NavComponent() {
   return (
     
-      <div className="px-16 py-4 flex flex-row justify-between bg-black ">
-        <div className="text-3xl font-[700]">
+      <div className="lg:px-16 py-4 flex flex-row justify-between bg-black ">
+        <div className=" text-2xl lg:text-3xl font-[700]">
           SK<span className="text-blue-700">.</span>
         </div>
-        <div className="flex flex-row gap-12 text-xl font-Poppins font-semibold">
+        <div className="hidden lg:flex flex-row gap-12 text-xl font-Poppins font-semibold">
           <button>Portfolio</button>
           <button>Skills</button>
           <button>Projects</button>
           <button>Contact</button>
+        </div>
+        <div className="lg:hidden">
 
         </div>
       </div>
